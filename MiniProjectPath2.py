27455e8caecac7941cef836adccc5534401c3437
atalegao <143094157+atalegao@users.noreply.github.com>
2024-04-21 20:50:42 -0400

    organized into functions

--- MiniProjectPath2.py
+++ MiniProjectPath2.py
@@ -1,26 +1,31 @@
 import pandas
-data = pd.read_csv("nyc_bicycle_counts_2016.csv")
-
-brooklyn_traffic = data['Brooklyn Bridge']
-manhattan_traffic = data['Manhattan Bridge']
-williamsburg_traffic = data['Williamsburg Bridge']
-queensboro_traffic = data['Queensboro Bridge']
-
-brooklyn_total = brooklyn_traffic.sum()
-manhattan_total = manhattan_traffic.sum()
-williamsburg_total = williamsburg_traffic.sum()
-queensboro_total = queensboro_traffic.sum()
+if __name__ == "__main__":
+    data = pandas.read_csv("nyc_bicycle_counts_2016.csv")
+    selected_bridges = highest_traffic(data)
 
 
-bridge_totals = {
-    'Brooklyn Bridge': brooklyn_total,
-    'Manhattan Bridge': manhattan_total,
-    'Williamsburg Bridge': williamsburg_total,
-    'Queensboro Bridge': queensboro_total
-}
-sorted_bridges = sorted(bridge_totals, key=bridge_totals.get, reverse=True)
-selected_bridges = sorted_bridges[:3]
-print("Selected bridges:", selected_bridges)
+def highest_traffic(data):
+
+    brooklyn_traffic = data['Brooklyn Bridge']
+    manhattan_traffic = data['Manhattan Bridge']
+    williamsburg_traffic = data['Williamsburg Bridge']
+    queensboro_traffic = data['Queensboro Bridge']
+
+    brooklyn_total = brooklyn_traffic.sum()
+    manhattan_total = manhattan_traffic.sum()
+    williamsburg_total = williamsburg_traffic.sum()
+    queensboro_total = queensboro_traffic.sum()
+
+
+    bridge_totals = {
+        'Brooklyn Bridge': brooklyn_total,
+        'Manhattan Bridge': manhattan_total,
+        'Williamsburg Bridge': williamsburg_total,
+        'Queensboro Bridge': queensboro_total
+    }
+    sorted_bridges = sorted(bridge_totals, key=bridge_totals.get, reverse=True)
+    selected_bridges = sorted_bridges[:3]
+    return selected_bridges
 ''' 
 The following is the starting code for path2 for data reading to make your first step easier.
 'dataset_2' is the clean data for path1.
