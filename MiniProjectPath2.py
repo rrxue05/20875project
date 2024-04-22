1b04fdc8c4c843d22fc97805891c94ee1870a2c9
atalegao <143094157+atalegao@users.noreply.github.com>
2024-04-21 20:18:39 -0400

    Add files via upload

---
+++ MiniProjectPath2.py
@@ -0,0 +1,34 @@
+import pandas
+data = pd.read_csv("nyc_bicycle_counts_2016.csv")
+
+brooklyn_traffic = data['Brooklyn Bridge']
+manhattan_traffic = data['Manhattan Bridge']
+williamsburg_traffic = data['Williamsburg Bridge']
+queensboro_traffic = data['Queensboro Bridge']
+
+brooklyn_total = brooklyn_traffic.sum()
+manhattan_total = manhattan_traffic.sum()
+williamsburg_total = williamsburg_traffic.sum()
+queensboro_total = queensboro_traffic.sum()
+
+
+bridge_totals = {
+    'Brooklyn Bridge': brooklyn_total,
+    'Manhattan Bridge': manhattan_total,
+    'Williamsburg Bridge': williamsburg_total,
+    'Queensboro Bridge': queensboro_total
+}
+sorted_bridges = sorted(bridge_totals, key=bridge_totals.get, reverse=True)
+selected_bridges = sorted_bridges[:3]
+print("Selected bridges:", selected_bridges)
+''' 
+The following is the starting code for path2 for data reading to make your first step easier.
+'dataset_2' is the clean data for path1.
+'''
+dataset_2 = pandas.read_csv('NYC_Bicycle_Counts_2016.csv')
+dataset_2['Brooklyn Bridge']      = pandas.to_numeric(dataset_2['Brooklyn Bridge'].replace(',','', regex=True))
+dataset_2['Manhattan Bridge']     = pandas.to_numeric(dataset_2['Manhattan Bridge'].replace(',','', regex=True))
+dataset_2['Queensboro Bridge']    = pandas.to_numeric(dataset_2['Queensboro Bridge'].replace(',','', regex=True))
+dataset_2['Williamsburg Bridge']  = pandas.to_numeric(dataset_2['Williamsburg Bridge'].replace(',','', regex=True))
+dataset_2['Williamsburg Bridge']  = pandas.to_numeric(dataset_2['Williamsburg Bridge'].replace(',','', regex=True))
+# print(dataset_2.to_string()) #This line will print out your data
